73f9edb79a6baef8fb1e7c06f290fb32cf3ffe34
ArtomMrdn <artommrdn47@gmail.com>
2023-02-01 22:12:11 +0200

    todo component updated

--- src/components/Todos/Todo.js
+++ src/components/Todos/Todo.js
@@ -3,11 +3,7 @@ import styles from "./Todo.module.css";
 
 function Todo({ todo, deleteTodo }) {
   return (
-    <div
-      className={`${styles.todo} ${
-        todo.isCompleted ? styles.completedTodo : ""
-      }`}
-    >
+    <div className={`${styles.todo} ${styles.completedTodo}`}>
       <div className={styles.todoText}>{todo.text}</div>
       <AiFillDelete
         className={styles.deleteIcon}
